0bf9cd0ffc1e9d73fda07ca900f5766621f0e6b8
Ibán Domínguez <domingueznoda@gmail.com>
2025-09-14 23:33:54 +0100

    Added useCredentials for ngrok deployment

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
     VitePWA({
       registerType: "prompt",
       injectRegister: false,
+      useCredentials: true,
 
       pwaAssets: {
         disabled: false,
